d575d3453b0c56b9949a69199a14d51e81b26d31
highonmelatonin <tanruianh@outlook.com>
2026-06-16 22:32:22 +0800

    Implemented loop

--- source/main.c
+++ source/main.c
@@ -12,27 +12,30 @@ int main(void)
     read_command(cmd); // Read a command from the user
 
     // If the command is "exit", break out of the loop to terminate the shell
-    if (strcmp(cmd[0], "exit") == 0)
-        // break;
-        return 0;
+    while (strcmp(cmd[0], "exit") != 0){
+        // Formulate the full path of the command to be executed
+        char full_path[PATH_MAX];
+        char cwd[1024];
+        if (getcwd(cwd, sizeof(cwd)) != NULL)
+        {
 
-    // Formulate the full path of the command to be executed
-    char full_path[PATH_MAX];
-    char cwd[1024];
-    if (getcwd(cwd, sizeof(cwd)) != NULL)
-    {
+            snprintf(full_path, sizeof(full_path), "%s/bin/%s", cwd, cmd[0]);
+        }
+        else
+        {
+            printf("Failed to get current working directory.");
+            exit(1);
+        }
 
-        snprintf(full_path, sizeof(full_path), "%s/bin/%s", cwd, cmd[0]);
+        execv(full_path, cmd);
+
+        // If execv returns, command execution has failed
+        printf("Command %s not found\n", cmd[0]);
+
+        type_prompt();
+        // To do: clear cmd so it doesn't repeat
+        read_command(cmd);
     }
-    else
-    {
-        printf("Failed to get current working directory.");
-        exit(1);
-    }
 
-    execv(full_path, cmd);
-
-    // If execv returns, command execution has failed
-    printf("Command %s not found\n", cmd[0]);
     exit(0);
 }
